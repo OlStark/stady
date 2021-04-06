a9ee242f44f957f4948d6a13b9dabfd0d3afd706
Oleg Starkov <starik293@gmail.com>
2021-04-06 11:41:50 +0300

    Урок №2

---
+++ lesson02/script.js
@@ -0,0 +1,24 @@
+let money = 30000;
+console.log(typeof money);
+
+let income = 'Фриланс';
+console.log(typeof income);
+
+let addExpenses = 'Еда, Трансспорт, Развлечения';
+console.log(addExpenses.length);
+let deposit = true;
+console.log(typeof deposit);
+
+let mission = 500000;
+
+let period = 9;
+
+console.log('Период равен' ,period, 'месяцев');
+console.log('Цель заработать' ,mission, 'рублей.');
+
+console.log(addExpenses.toUpperCase());
+console.log(addExpenses.toLowerCase());
+console.log(addExpenses.split(' '));
+
+let budgetDay = money/30;
+console.log(budgetDay);
