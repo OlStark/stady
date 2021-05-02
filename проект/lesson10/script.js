a12bc07486d9ea841f508db28af03372edba935c
Oleg Starkov <starik293@gmail.com>
2021-05-02 18:22:28 +0300

    Дз10

--- проект/lesson10/script.js
+++ проект/lesson10/script.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 let books = document.querySelectorAll('.books'),
     book = document.querySelectorAll('.book');
@@ -14,10 +14,28 @@ books[0].append(book[2]);
 let chengedHeader = document.getElementsByTagName('a')[2];
 chengedHeader.replaceWith('Книга 3. this и Прототипы Объектов');
 
+//Поменял фон//
+document.querySelector('body').style.backgroundImage = 'url(./image/you-dont-know-js.jpg)';
+
+//Изменил порядок глав в 5 книге//
+
+let elems = document.querySelectorAll('.book')[4].querySelectorAll('li');
+
+elems[4].after(elems[2]);
+elems[3].before(elems[9]);
+elems[7].after(elems[5]);
 
 
-console.log(books);
-console.log(book);
+//Добавил главу в 6 книгу//
+
+let heads = document.querySelectorAll('.book')[5].querySelectorAll('li'),
+    newHead = document.createElement('li');
+
+newHead.textContent = 'Глава 8: За пределами ES6';
+
+heads[8].after(newHead);
+
+
 //Удалил рекламу//
 let spam = document.querySelector('.adv');
 spam.remove();
